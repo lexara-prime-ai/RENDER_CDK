328962bad0e017816debc8b2703983fa57b2f114
lexara-prime-ai <irfanghat@gmail.com>
2024-07-09 14:48:20 +0000

    Updated <audit.yaml> path

--- rust/src/state/state.rs
+++ rust/src/state/state.rs
@@ -3,6 +3,9 @@ use anyhow::{Context, Error, Ok, Result};
 use reqwest::header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE};
 use reqwest::{self};
 
+#[allow(non_snake_case)]
 pub struct State {
-    
+    CLIENT: reqwest::Client,
 }
+
+
