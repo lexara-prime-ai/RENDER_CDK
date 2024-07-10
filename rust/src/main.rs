cf39581df2b50f770e1d3c91b271a7a06448fa26
lexara-prime-ai <irfanghat@gmail.com>
2024-07-10 06:59:50 +0000

    Added method -> <find_service_by_region>

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -8,8 +8,11 @@ use tokio::main;
 #[main]
 async fn main() {
     /// List all Services.
-    let services = ServiceManager::list_all_services("20").await;
+    // let services = ServiceManager::list_all_services("20").await;
 
     // List all Services by Name and Type.
     // let services = ServiceManager::find_service_by_name_and_type("whoami", "web_service").await;
+
+    // 'https://api.render.com/v1/services?region=oregon&limit=20'
+    let services = ServiceManager::find_service_by_region("oregon", "10").await;
 }
