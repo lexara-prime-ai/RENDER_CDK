5b7f23cde7c3bfe26cd0e2292327d4f5a5e8eeb2
lexara-prime-ai <irfanghat@gmail.com>
2024-07-14 08:39:16 +0000

    Updated blueprints.

--- rust/src/iaas/blueprints/generate.rs
+++ rust/src/iaas/blueprints/generate.rs
@@ -2,13 +2,17 @@
 #![allow(unused)]
 
 #[derive(Debug, Clone)]
-pub struct ServiceConf {
+pub struct BaseServiceConf {
     pub service_type: String,
     pub service_name: String,
     pub service_runtime: String,
     pub service_plan: String,
 }
 
+// #[derive(Debug, Clone)]
+// pub struct Image
+
+#[derive(Debug, Clone)]
 pub struct Blueprints {
-    pub base_conf: ServiceConf,
+    pub base_conf: BaseServiceConf,
 }
