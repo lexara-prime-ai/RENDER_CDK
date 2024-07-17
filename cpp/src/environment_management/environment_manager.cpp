6885de9e08da522a2e31c1a914577f32f33eb456
lexara-prime-ai <irfanghat@gmail.com>
2024-07-17 22:13:51 +0000

    Code refectoring!

---
+++ cpp/src/environment_management/environment_manager.cpp
@@ -0,0 +1,17 @@
+#include "../../include/environment_manager.h"
+#include "../../include/dotenv.h"
+#include <cstdlib>
+#include <iostream>
+
+const char *API_KEY_PATH = "cpp.env";
+
+std::string EnvironmentManager::getApiKey() {
+  dotenv::init(API_KEY_PATH);
+  const std::string apiKey = std::getenv("API_KEY");
+
+  if (!apiKey.empty()) {
+    return std::string(apiKey);
+  } else {
+    return "[API_KEY] must be set.";
+  }
+}
