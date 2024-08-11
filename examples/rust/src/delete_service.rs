3c401a2d0f1758c1dbffc588b30eb2a3c262adaa
lexara-prime-ai <irfanghat@gmail.com>
2024-08-11 10:34:28 +0000

    Updated markdoown docs, bump crate version.

---
+++ examples/rust/src/delete_service.rs
@@ -0,0 +1,37 @@
+use render_cdk::resource_management::prelude::*;
+use tokio::main;
+use tokio::time::{sleep, Duration};
+
+#[main]
+async fn main() {
+    //! This example demostrates how to create
+    //!   a service and delete it.
+    //!
+    //!   Pre-requisites
+    //!   - A valid [API_KEY] from Render Cloud.
+    let deployment_config = Static {
+        type_: "static_site".to_owned(),
+        name: "test_deployment".to_owned(),
+        repo: "https://github.com/lexara-prime-ai/SAMPLE_STATIC_SITE".to_owned(),
+        auto_deploy: "yes".to_owned(), // By default, Render automatically deploys your service whenever you update its code or configuration.
+        root_dir: Some("./public".to_owned()),
+        service_details: Some(ServiceDetails {
+            build_command: None, // Render runs this command to build your app before each deploy e.g npm run build, yarn build.
+            publish_path: Some("./".to_owned()), // This will translate to /public/
+            pull_request_previews_enabled: Some("yes".to_owned()),
+            ..Default::default()
+        }),
+        ..Default::default()
+    };
+
+    // Create and Deploy the service.
+    ServiceManager::create_static_site(deployment_config)
+        .await
+        .unwrap();
+
+    // Wait until the service is deployed.
+    sleep(Duration::from_secs(60)).await;
+
+    // Deleting services.
+    ServiceManager::delete_service("test_deployment", "static").await;
+}
--- examples/rust/src/list_all_services.rs
+++ examples/rust/src/list_all_services.rs
@@ -1,12 +1,11 @@
-use render_cdk::resource_management::{prelude::*};
+use render_cdk::resource_management::prelude::*;
 use tokio::main;
 
-  
 #[main]
 async fn main() {
     //! This example demostrates how to retrieve
     //!   a list of services, with a specified limit.
-    //! 
+    //!
     //!   Pre-requisites
     //!   - A valid [API_KEY] from Render Cloud.
     ServiceManager::list_all_services("50").await.unwrap();
